e678957191ea835a72488ba55276b3eca7de0dc2
Christian Axel Serrano Sandoval ( Chiles ) <61697287+Chiles012@users.noreply.github.com>
2023-01-25 20:11:26 -0600

    dom

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,29 @@
+const butonCancel = document.querySelector('.buton-cancel')
+const buton2 = document.querySelectorAll('.buton')[1];
+const date = document.querySelector('#date')
 
-document.addEventListener("DOMContentLoaded", () => {
-    
-    const user = localStorage.getItem("user");
+document.addEventListener("DOMContentLoaded", function() {
 
-    if (user) {
-        // TODO: obtener usuario
-    } else {
-        window.location = "/login.html"
-    }
+    const paragraph = document.createElement('p');
+    const text = document.createTextNode('Hola mundo!');
+
+    paragraph.appendChild(text);
+
+    butonCancel.appendChild(paragraph);
+
+    butonCancel.classList.add('buton');
+
+    date.addEventListener('change', function(e) {
+        console.log(e.target.value);
+    });
+
+    buton2.addEventListener("click", function(e) {
+        console.log(e)
+        console.log('boton 2');
+    })
 
 });
+
+function alertJS(message) {
+    alert(message);
+}
