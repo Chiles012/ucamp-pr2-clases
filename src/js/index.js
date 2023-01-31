69ed5254c9ad656414eed0ce659ccbfad9ff77c4
Christian Axel Serrano Sandoval ( Chiles ) <61697287+Chiles012@users.noreply.github.com>
2023-01-30 20:01:20 -0600

    eliminar

--- src/js/index.js
+++ src/js/index.js
@@ -15,14 +15,12 @@ document.addEventListener("DOMContentLoaded", function() {
     } else {
         if (nombres.length > 0) {
             for ( let i = 0; i < nombres.length; i++ ) {
-                const p = document.createElement('p');
-                const text = document.createTextNode(nombres[i])
-                p.appendChild(text)
-                content.appendChild(p)
+                showNames(nombres, i)
             }
         } else {
             const p = document.createElement('p');
             const text = document.createTextNode('No hay elementos')
+
             p.appendChild(text)
             content.appendChild(p)
         }
@@ -37,11 +35,42 @@ document.addEventListener("DOMContentLoaded", function() {
         content.innerHTML = '';
 
         for ( let i = 0; i < nombres.length; i++ ) {
-            const p = document.createElement('p');
-            const text = document.createTextNode(nombres[i])
-            p.appendChild(text)
-            content.appendChild(p)
+            showNames(nombres, i);
         }
     })
    
 });
+
+function showNames(nombres, i) {
+    const div = document.createElement('div')
+    const p = document.createElement('p');
+    const text = document.createTextNode(nombres[i])
+    p.appendChild(text)
+
+    const button = document.createElement('button');
+    const textButton = document.createTextNode("Eliminar");
+    button.appendChild(textButton);
+    button.onclick = () => {
+        eliminar(i)
+    }
+
+    div.appendChild(p);
+    div.appendChild(button);
+
+    content.appendChild(div);
+}
+
+function eliminar(i) {
+
+    var nombres = JSON.parse(localStorage.getItem('nombres')) || [];
+    nombres.splice(i, 1);
+
+    localStorage.setItem('nombres', JSON.stringify(nombres));
+
+    content.innerHTML = "";
+
+    for (let j = 0; j < nombres.length; j++) {
+        showNames(nombres, j);
+    }
+    
+}
